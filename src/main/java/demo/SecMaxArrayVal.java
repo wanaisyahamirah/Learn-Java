98a5eb57ba62a83d867274f59810d505eb1e846a
Wan Aisyah Amirah <aisyahamirahzamri@gmail.com>
2022-12-27 17:17:27 +0800

    Remove unecessary Hello World

--- src/main/java/demo/SecMaxArrayVal.java
+++ src/main/java/demo/SecMaxArrayVal.java
@@ -1,32 +1,30 @@
 package demo;
 
-public class SecMaxArrayVal 
-{
-    public static void main( String[] args )
-    {
+public class SecMaxArrayVal {
+    public static void main(String[] args) {
 
-        int array[] = {10, 20, 25, 63, 96, 57};
+        int array[] = { 10, 20, 25, 63, 96, 57 };
 
-        boolean isUnique=isUniqueArray(array);
+        boolean isUnique = isUniqueArray(array);
 
-        if(!isUnique){
-			return;
-		}
-		System.out.println("Is Unique is true");
-		if(!isAtLeastTwoVal(array)){
-			return;
-		}
-		System.out.println("Have Two Values is true ");
+        if (!isUnique) {
+            return;
+        }
+        System.out.println("Is Unique is true");
+        if (!isAtLeastTwoVal(array)) {
+            return;
+        }
+        System.out.println("Have Two Values is true ");
 
-		System.out.println("Second Largest Value  is :: " +findSecLargestVal(array));
+        System.out.println("Second Largest Value  is :: " + findSecLargestVal(array));
     }
-   
-    public static boolean isUniqueArray(int arrayValue[])
-    {
+
+    public static boolean isUniqueArray(int arrayValue[]) {
 
         for (int i = 0; i < arrayValue.length; i++) {
             for (int j = 0; j < arrayValue.length; j++) {
-                if (i == j) continue; // when sama index, no need to compare
+                if (i == j)
+                    continue; // when sama index, no need to compare
                 if (arrayValue[i] == arrayValue[j]) { // return false kalau sama, maksudnya tak unik
                     return false;
                 }
@@ -36,27 +34,26 @@ public class SecMaxArrayVal
 
     }
 
-    public static boolean isAtLeastTwoVal(int arrayValue[]){
+    public static boolean isAtLeastTwoVal(int arrayValue[]) {
         return arrayValue.length >= 2;
     }
 
-    public static int findSecLargestVal(int arrayValue[]){
+    public static int findSecLargestVal(int arrayValue[]) {
 
         int temp, size;
         size = arrayValue.length;
-        
-        
-        for(int i = 0; i<size; i++ ){
-           for(int j = i+1; j<size; j++){
-  
-              if(arrayValue[i]>arrayValue[j]){
-                 temp = arrayValue[i];
-                 arrayValue[i] = arrayValue[j];
-                 arrayValue[j] = temp;
-              }
-           }
+
+        for (int i = 0; i < size; i++) {
+            for (int j = i + 1; j < size; j++) {
+
+                if (arrayValue[i] > arrayValue[j]) {
+                    temp = arrayValue[i];
+                    arrayValue[i] = arrayValue[j];
+                    arrayValue[j] = temp;
+                }
+            }
         }
-        return arrayValue[size-2];
+        return arrayValue[size - 2];
     }
 
 }
