42e16b6948e33777dbc221d454385d9c4b5ffbf0
Wan Aisyah Amirah <aisyahamirahzamri@gmail.com>
2022-12-29 14:17:06 +0800

    add code to test ReversedSentence.java

--- src/test/java/demo/ReversedSentenceTest.java
+++ src/test/java/demo/ReversedSentenceTest.java
@@ -2,6 +2,9 @@ package demo;
 
 import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 
 public class ReversedSentenceTest 
@@ -10,6 +13,17 @@ public class ReversedSentenceTest
     @Test
     public void splitInputBySpaceTest()
     {
-        assertTrue( true );
+
+        String expectedInput = "Hello World";
+
+        List<String> expectedList=new ArrayList<>();
+		expectedList.add("Hello");
+		expectedList.add("World");
+
+        List<String> resultList = ReversedSentence.splitInputBySpace(expectedInput) ;
+
+        assertTrue(expectedList.get(0).equals(resultList.get(0)));
+		assertTrue(expectedList.get(1).equals(resultList.get(1)));
     }
+
 }
